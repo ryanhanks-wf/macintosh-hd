d87ee649ad8687ca85206c9f9d54540f86800f75
Ryan Hanks <ryandhanks@gmail.com>
2016-05-01 05:23:41 -0700

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+GITHUB_USER=ryanhanks
+REPO=mac-hd
+
+# Initialize sudo
+sudo -v
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Install ansible
+sudo easy_install pip==1.5.6
+sudo pip install ansible
+
+# Make sure we have proper access to /usr/local
+sudo chmod -R g+rwx /usr/local
+sudo chgrp -R admin /usr/local
+
+# Create a space for storing our source code
+mkdir -p src/github.com/$GITHUB_USER
+pushd src/github.com/$GITHUB_USER
+
+# Clone the config repo
+git clone git@github.com:$USER/$REPO
+
+# Run ansible against the config
+pushd $REPO
+ansible-galaxy install -r requirements.yml
+ansible-playbook -i "localhost," main.yml

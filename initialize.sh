8a83fdba286bbba29bc87c7a0a106729d0067cf0
Ryan Hanks <ryandhanks@gmail.com>
2016-05-01 04:59:30 -0700

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo -v
 sudo -s 'ssh-keyscan -H github.com > /etc/ssh/ssh_known_hosts'
-ssh-keygen -t rsa
+ssh-keygen -t rsa -f ~/.ssh/id_rsa.pub
 pbcopy < ~/.ssh/id_rsa.pub
 open 'https://github.com/settings/keys'
 xcode-select --install

d252fe2ac01f06878a7abda00233fb4c1daa4cea
Ryan Hanks <ryan.hanks@workiva.com>
2016-05-05 13:20:24 -0700

    Add dart to post-install

--- post-bootstrap.sh
+++ post-bootstrap.sh
@@ -4,3 +4,5 @@ npm install -g phantomjs@1.9.1-0
 npm install -g bower
 npm install -g grunt-cli
 sudo gem install compass-rgbapng
+brew tap dart-lang/dart
+brew install dart --with-content-shell --with-dartium

315db6ca44e8e3ed56f686a72fea2379b91bd60b
ryanhanks-wf <ryan.hanks@workiva.com>
2016-05-06 15:53:42 -0700

    Update post-bootstrap.sh

--- post-bootstrap.sh
+++ post-bootstrap.sh
@@ -4,5 +4,6 @@ npm install -g phantomjs@1.9.1-0
 npm install -g bower
 npm install -g grunt-cli
 sudo gem install compass-rgbapng
-brew tap dart-lang/dart
-brew install dart --with-content-shell --with-dartium
+brew tap ryanhanks-wf/dart
+brew install dart1150 --with-content-shell --with-dartium
+brew install nginx

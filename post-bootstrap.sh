76d8d2f3c3169f8a54f93bcc264fe6c9b0066574
Ryan Hanks <ryan.hanks@workiva.com>
2016-05-09 22:42:31 -0700

    Way too much stuff

--- post-bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-sudo pip install virtualenv
-npm install -g phantomjs@1.9.1-0
-npm install -g bower
-npm install -g grunt-cli
-sudo gem install compass-rgbapng
-brew tap ryanhanks-wf/dart
-brew install dart1150 --with-content-shell --with-dartium
-brew install nginx

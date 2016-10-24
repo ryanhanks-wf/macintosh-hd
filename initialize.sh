b11f6afd8e3a049a517577fd300550653c4f73f4
ryanhanks-wf <ryan.hanks@workiva.com>
2016-10-24 14:57:25 -0700

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -4,7 +4,10 @@
 set -e
 
 # Add github to known hosts
-sudo -s 'ssh-keyscan -H github.com > /etc/ssh/ssh_known_hosts'
+echo "#!/bin/bash\nssh-keyscan -H github.com > /etc/ssh/ssh_known_hosts" > update_known_hosts.sh
+chmod 755 update_known_hosts.sh
+sudo update_known_hosts.sh
+rm update_known_hosts.sh
 
 # Generate ssh key, copy to clipboard, open github keys for adding new key
 ssh-keygen -t rsa -f ~/.ssh/id_rsa
